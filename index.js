e0e9af72393047a3ed00abaf697c43a20a748eeb
Evelyn Bauer <evybauer@gmail.com>
2019-12-06 04:50:25 +0000

    fetchCoordsByIP Function added

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-// // fetchMyIP((error, ip) => {
-// //   if (error) {
-// //     console.log("It didn't work!" , error);
-// //     return;
-// //   }
 
-// //   console.log('It worked! Returned IP:' , ip);
-// // });
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+
+fetchCoordsByIP('162.245.144.188', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned Coords:' , coords);
+});
